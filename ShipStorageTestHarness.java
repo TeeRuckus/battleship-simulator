9bae77a9e9ff2c792bbb5daba9b9b61e31729ffb
Tawana <tawanakwaramba@gmail.com>
2019-05-12 23:58:30 +0800

    creating test harnesses

--- ShipStorageTestHarness.java
+++ ShipStorageTestHarness.java
@@ -12,11 +12,11 @@ public class ShipStorageTestHarness
             shipStorage = new ShipStorage();
             subs[0] = new SubMarine(101.301, 2000, SubMarine.STEEL, -300);
             subs[1] = new SubMarine(200.500, 1969, SubMarine.TTNM, -250);
-            subs[2] = new SubMarine();
+            subs[2] = subs[0].clone();
             subs[3] = new SubMarine(200.600, 1955, SubMarine.ALLY, -1);
 
-            jets[0] = new FighterJet();
-            jets[1] = new FighterJet(101.302, 2000, "teddy bears", 20);
+            jets[0] = new FighterJet(101.302, 2000, "teddy bears", 20);
+            jets[1] = jets[0].clone();
             jets[2] = new FighterJet(199.666, 1969, "ninjas", 4);
             jets[3] = new FighterJet(100.300, 1955, "OOPD is insane", 5);
 
@@ -36,11 +36,16 @@ public class ShipStorageTestHarness
                                 +subs[3].toString()+"\n");
             System.out.println("\nget sub method: \n"+
                                "\n"+shipStorage.getSub().toString()+"\n");
-
+            /* something is going wrong here 
             System.out.println("\nlast jet stored: \n\n" 
-                                +jets[3].toString()+"\n");
+                                +jets[3].toString()+"\n");*/
             System.out.println("\nget jet method: \n"+
                                "\n" +shipStorage.getLastJet().toString()+"\n");
+
+            System.out.println("\n FIND DUPLICATES TEST \n");
+
+            
+            
             
         }
         catch(IllegalArgumentException err)
