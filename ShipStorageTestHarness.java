8ff69da88c0ab6f65397062cf526e05fd1a3e900
Tawana <tawanakwaramba@gmail.com>
2019-05-20 03:50:16 +0800

    Testing code

--- ShipStorageTestHarness.java
+++ ShipStorageTestHarness.java
@@ -10,19 +10,19 @@ public class ShipStorageTestHarness
         
             //creating objects for testing
             shipStorage = new ShipStorage();
-            subs[0] = new SubMarine(101.301, 2000, SubMarine.STEEL, -300, new
-                                    Engine());
-            subs[1] = new SubMarine(200.500, 1969, SubMarine.TTNM, -250, new
-                                    Engine(4,"BIO"));
+            subs[0] = new SubMarine("101.301", 2000, new Engine(),
+                                    SubMarine.ALLY, -300);
+            subs[1] = new SubMarine("200.500", 1969, new Engine(4,"BIO"),
+                                    SubMarine.TTNM, -200);
             subs[2] = subs[0].clone();
-            subs[3] = new SubMarine(200.600, 1955, SubMarine.ALLY, -1, new
-                                    Engine(16, "DIESEL"));
+            subs[3] = new SubMarine("200.600", 1955, new Engine(16, "DIESEL"),
+                                     SubMarine.ALLY, -1);
 
-            jets[0] = new FighterJet(101.302, 2000, "teddy bears", 20, new
+            jets[0] = new FighterJet("101.302", 2000, "teddy bears", 20, new
                                      Engine(18,"BATTERY"));
             jets[1] = jets[0].clone();
-            jets[2] = new FighterJet(199.666, 1969, "ninjas", 4, new Engine());
-            jets[3] = new FighterJet(100.300, 1955, "OOPD is insane", 5, new
+            jets[2] = new FighterJet("199.666", 1969, "ninjas", 4, new Engine());
+            jets[3] = new FighterJet("100.300", 1955, "OOPD is insane", 5, new
                                      Engine(4,"BIO"));
             SubMarine[] duplicatesSubs = new 
                                          SubMarine[shipStorage.MAX_CAP * 2];
@@ -32,11 +32,11 @@ public class ShipStorageTestHarness
             //ADD SHIPS TESTING
             for(int ii = 0; ii < 4; ii++)
             {
-                shipStorage.addShipSub(subs[ii]);
+                shipStorage.addShip(subs[ii]);
             }
             for(int ii = 0; ii < 4; ii++)
             {
-                shipStorage.addShipJet(jets[ii]);
+                shipStorage.addShip(jets[ii]);
             }
 
             System.out.println("\n GET LAST SHIP STORED TEST \n");
@@ -44,15 +44,15 @@ public class ShipStorageTestHarness
             System.out.println("\nlast sub stored: \n\n" 
                                 +subs[3].toString()+"\n");
             System.out.println("\nget sub method: \n"+
-                               "\n"+shipStorage.getSub().toString()+"\n");
+                               "\n"+shipStorage.getShip().toString()+"\n");
             System.out.println("\nlast jet stored: \n\n" 
                                 +jets[3].toString()+"\n");
             System.out.println("\nget jet method: \n"+
-                               "\n" +shipStorage.getLastJet().toString()+"\n");
+                               "\n" +shipStorage.getShip().toString()+"\n");
 
             System.out.println("\n FIND DUPLICATES TEST \n");
             
-            shipStorage.findDuplicateSubs();
+            shipStorage.findDuplicateShips();
             System.out.println("\n Submarine duplicates \n"); 
             for(int ii = 0; ii < 4; ii++)
             {
