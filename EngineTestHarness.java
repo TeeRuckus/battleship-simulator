fb59ae9d39793ff2740e1e87090e9edcb49c34de
Tawana <tawanakwaramba@gmail.com>
2019-05-19 23:26:24 +0800

    testing Engine.java

--- EngineTestHarness.java
+++ EngineTestHarness.java
@@ -1,5 +1,9 @@
 public class EngineTestHarness
 {
+    public static final String BATRY = "BATTERY";
+    public static final String DSL = "DIESEL";
+    public static final String BIO = "BIO";
+
     public static void main(String [] args)
     {
         /*please refer to TestDesign.ods to get break down of the test cases 
@@ -33,11 +37,24 @@ public class EngineTestHarness
             System.out.println("\nGETTERS AND SETTERS:\n");
             engine[0].setCylinders(engine[1].getCylinders());
             System.out.println(engine[0].getCylinders() + " = " 
-                               + engine[1].getCylinders());
+                               + engine[ii].getCylinders());
 
             engine[0].setFuel(engine[1].getFuel());
             System.out.println(engine[0].getFuel() + " = " 
                                          + engine[1].getFuel());
+
+            //Private submodules
+
+            boolean actuall;
+            System.out.println("\nvalidateCylinders\n");
+            actual = validateCylinders(1);
+            assert actual == false: "inCylinders <= 2";
+
+            actual = validateCylinders(40);
+            assert actual == false: "inCylinders >= 20";
+
+            actual = validateCylinders(20);
+            assert actual == true: "2 > inCylinders < 20";
 
         }
         catch(IllegalArgumentException err)
@@ -46,4 +63,60 @@ public class EngineTestHarness
         }
 
     }
+        
+    /*private submodules cannot be tested by calling the class, hence I have to
+      copy and paste all of engine's private submodules here */
+
+        /***********************************************************************
+        SUBMODULE: validateCylinders
+        IMPORT: inCylinders (Real)
+        EXPORT: isValid (Boolean)
+        ASSERTION: validates serial numbers as true whereby its first three
+                   digits are between 100 to 200 (inclusive), and the last three
+                   digits are between 001 - 999 (inclusive), otherwise it will
+                   validate them as false.
+        ***********************************************************************/
+        private boolean validateCylinders(int inCylinders)
+        {
+            boolean isValid = false;
+
+            if(inCylinders >= 2 && inCylinders <= 20)
+            {
+                isValid = true;
+            }
+            else
+            {
+                /* I am commenting this out, so it doesn't stop my whole test
+                   programme 
+                throw new IllegalArgumentException("\nInvalid cylinders\n");*/
+                System.out.println("an IllegalArgumentException is meant "+
+                                   "to be thrown here");
+            }
+
+            return isValid;
+        }
+        /**********************************************************************
+        SUBMODULE: validateFuel
+        IMPORT: inFuel (String)
+        EXPORT: isValid (boolean)
+        ASSERTION: it will only validate inFuel if it has a string describing
+                   the fuel of the fighter jet
+        ********************************************************************/
+        private boolean validateFuel(String inFuel)
+        {
+            boolean isValid = false;
+
+            if(inFuel.equals(BATRY) || inFuel.equals(DSL) ||inFuel.equals(BIO))
+            {
+                isValid = true;
+            }
+            else
+            {
+                throw new IllegalArgumentException("\nInvalid fuel\n");
+            }
+
+            return isValid;
+        }
+
+
 }
